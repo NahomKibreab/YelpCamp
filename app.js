22be965a76bf87eaebf532ca5b7ae2fb9af7aa5e
NahomKibreab <nknahom@gmail.com>
2021-04-10 17:36:33 -0600

    A New EJS-Mate Tool For Layout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost/yelp-camp', {
   useNewUrlParser: true,
@@ -18,6 +19,7 @@ db.once('open', function () {
   console.log('Database connected!');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
